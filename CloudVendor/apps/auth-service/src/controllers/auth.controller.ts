b04349d494e265092b57034d617d0b9d080c4005
khadija1205 <khadijac101@gmail.com>
2025-09-14 19:58:05 +0530

    Refactor: Remove .env from repo

--- CloudVendor/apps/auth-service/src/controllers/auth.controller.ts
+++ CloudVendor/apps/auth-service/src/controllers/auth.controller.ts
@@ -1,21 +1,37 @@
-import { Request, Response,NextFunction } from "express";
-import { validateRegistrationData } from "../utils/auth.helper";
+import { Request, Response, NextFunction } from "express";
+import {
+  checkOtpRestrictions,
+  sendOtp,
+  trackOtpRequests,
+  validateRegistrationData,
+} from "../utils/auth.helper";
 import prisma from "../../../../packages/libs/prisma";
 import { ValidationError } from "../../../../packages/error-handler";
 
 // Register a new User
-export const userRegistration = async (req: Request, res: Response, next: NextFunction) => {
-
-    validateRegistrationData(req.body, 'user');
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validateRegistrationData(req.body, "user");
     const { name, email } = req.body;
 
-    const existingUser = await prisma.users.findUnique({ where: email });
+    const existingUser = await prisma.users.findUnique({ where: { email } });
 
     if (existingUser) {
-        return next(new ValidationError("User already exists with this email!"));
+      return next(new ValidationError("User already exists with this email!"));
     }
 
     await checkOtpRestrictions(email, next);
+    await trackOtpRequests(email, next);
+    await sendOtp(name, email, "user-activation-mail");
 
-
+    res.status(200).json({
+      message: "OTP sent to email. Please verify your account",
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
