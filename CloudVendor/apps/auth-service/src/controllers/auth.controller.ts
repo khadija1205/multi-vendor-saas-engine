61a368f716a24227ffb05da5a06b7f5804eb57c0
khadija1205 <khadijac101@gmail.com>
2025-09-16 00:51:54 +0530

    feat: hash password and store user in primary DB after verification

--- CloudVendor/apps/auth-service/src/controllers/auth.controller.ts
+++ CloudVendor/apps/auth-service/src/controllers/auth.controller.ts
@@ -1,45 +1,49 @@
-import { Request, Response,NextFunction } from "express";
-import { checkOtpRestrictions, sendOtp, trackOtpRequests, validateRegistrationData, verifyOtp } from "../utils/auth.helper";
+import { Request, Response, NextFunction } from "express";
+import {
+  checkOtpRestrictions,
+  sendOtp,
+  trackOtpRequests,
+  validateRegistrationData,
+  verifyOtp,
+} from "../utils/auth.helper";
 import prisma from "../../../../packages/libs/prisma";
 import { ValidationError } from "../../../../packages/error-handler";
+import bcrypt from "bcryptjs";
 
 // Register a new User
-export const userRegistration = async (req: Request, res: Response, next: NextFunction) => {
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validateRegistrationData(req.body, "user");
+    const { name, email } = req.body;
 
-    try {
+    const existingUser = await prisma.users.findUnique({ where: { email } });
 
-        validateRegistrationData(req.body, "user");
-        const { name, email } = req.body;
-
-        const existingUser = await prisma.users.findUnique({ where: email });
- 
-        if (existingUser) {
-          return next(
-            new ValidationError("User already exists with this email!")
-          );
-        }
-
-        await checkOtpRestrictions(email, next);
-        await trackOtpRequests(email, next);
-        await sendOtp(email, name, "user-activation-mail");
-
-        res.status(200).json({
-          message: "OTP sent to email. Please verify your account",
-        });
-        
+    if (existingUser) {
+      return next(new ValidationError("User already exists with this email!"));
     }
 
-    catch (error) {
+    await checkOtpRestrictions(email, next);
+    await trackOtpRequests(email, next);
+    await sendOtp(name, email, "user-activation-mail");
 
-        return next(error);
-        
-    }
-
+    res.status(200).json({
+      message: "OTP sent to email. Please verify your account",
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-
 // Verify user with otp
-export const verifyUser = async (req: Request, res: Response, next:NextFunction) => {
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { email, otp, password, name } = req.body;
     if (!email || !otp || !password || !name) {
@@ -53,10 +57,18 @@ export const verifyUser = async (req: Request, res: Response, next:NextFunction)
     }
 
     await verifyOtp(email, otp, next);
-    
-    
-  }
-  catch (error) {
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.users.create({
+      data: { name, email, password: hashedPassword },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfuly",
+    });
+  } catch (error) {
     return next(error);
   }
-}
+};
