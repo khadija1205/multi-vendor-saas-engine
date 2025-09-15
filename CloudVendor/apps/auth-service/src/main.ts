e1ec388ec109339898177e1d0d1fce0da6d773b8
khadija1205 <khadijac101@gmail.com>
2025-09-15 22:44:24 +0530

    fix: resolve syntax errors

--- CloudVendor/apps/auth-service/src/main.ts
+++ CloudVendor/apps/auth-service/src/main.ts
@@ -5,13 +5,6 @@ import cookieParser from "cookie-parser";
 import router from "./routes/auth.router";
 import swaggerUi from "swagger-ui-express";
 const swaggerDocument = require("./swagger-output.json");
-import express from "express";
-import cors from "cors";
-import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
-import cookieParser from "cookie-parser";
-import router from "./routes/auth.router";
-import swaggerUi from "swagger-ui-express";
-const swaggerDocument = require("./swagger-output.json");
 
 const app = express();
 
@@ -19,17 +12,6 @@ app.use(
   cors({
     origin: ["http://localhost:3000"],
     allowedHeaders: ["Authorization", "Content-Type"],
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-
-app.use(cookieParser());
   })
 );
 
@@ -39,19 +21,7 @@ app.use(cookieParser());
 
 app.get("/api", (req, res) => {
   res.send({ message: "Hello API" });
-app.get("/api", (req, res) => {
-  res.send({ message: "Hello API" });
 });
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.get("/docs-json", (req, res) => {
-  res.json(swaggerDocument);
-});
-
-// Routes
-app.use("/api", router);
-
-app.use(errorMiddleware);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/docs-json", (req, res) => {
@@ -71,6 +41,4 @@ const server = app.listen(port, () => {
 
 server.on("error", (err) => {
   console.log("Server Error:", err);
-  console.log("Server Error:", err);
 });
-
